f12b314486282f38404e53cd416406f433973641
lubenard <lubenard@student.42.fr>
2020-04-25 19:45:43 +0200

    Upgraded version of app

--- app/src/main/java/com/karltrick/maisditmoi/MainActivity.java
+++ app/src/main/java/com/karltrick/maisditmoi/MainActivity.java
@@ -1,7 +1,6 @@
 package com.karltrick.maisditmoi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,14 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button one = this.findViewById(R.id.button1);
+        Button one = this.findViewById(R.id.sound);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.sound);
         one.setOnClickListener(new View.OnClickListener(){
 
@@ -29,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button two = this.findViewById(R.id.button2);
+        Button two = this.findViewById(R.id.video);
         final VideoView myVideoView = (VideoView) findViewById(R.id.videoView);
         myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video));
 
